10f218bc33715a79a01321f7e7268df88392cce0
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-13 02:31:29 +0000

    calculo completo

---
+++ CalculandoMedia.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+// Programa para calculo de média
+int main(){
+
+ int nota1,nota2, nota3;
+ float media;
+
+ printf("Escreva a primeira nota: \n");
+ scanf("%d\n",nota1);
+
+ printf("Escreva a segunda nota: \n");
+ scanf("%d\n",nota2);
+
+ printf("Escreva a terceira nota: \n");
+ scanf("%d\n",nota3);
+
+ media = (float)(nota1 + nota2 + nota3) / 3;
+
+
+ printf("A media e: %1.f \n", media);
+
+
+ return 0;
+
+
+
+
+
+}
