be0e3d56736a811030223b6ebba9593044b412c8
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-16 22:11:33 +0000

    condição

---
+++ Conficao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int a = 20;
+    int b = 12;
+
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+
+
+
+return 0;
+
+
+
+
+}
