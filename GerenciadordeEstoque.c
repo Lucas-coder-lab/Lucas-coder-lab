033c640afa73fa947dd2dfbbb2043dad2a590f46
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-16 23:04:58 +0000

    finalizadoGerenciamento

--- GerenciadordeEstoque.c
+++ GerenciadordeEstoque.c
@@ -4,8 +4,8 @@ int main(){
 
  //variaveis para produto, estoque e valores.
 
-char produtoA [30];
-char produtoB [30];
+char produtoA [30] = "Produto A";
+char produtoB [30] = "Produto A";
 
 unsigned int estoqueA = 1000;
 unsigned int estoqueB = 2000;
@@ -14,15 +14,25 @@ float valorA = 10.50;
 float valorB = 20.40;
 
 unsigned int estoqueminimoA = 500;
-unsigned int estoqueminimoA = 2500;
+unsigned int estoqueminimoB = 2500;
 
 double valortotalA;
 double valortotalB;
 
+int resultadoA, resultadoB;
 
-printf("Produto %s tem estoque %u e o valor unitario é %f\n", produtoA, estoqueA, valorA);
-printf("Produto %s tem estoque %u e o valor unitario é %f\n", produtoB, estoqueB, valorB);
+//Exibir informações
 
+printf("Produto %s tem estoque %u e o valor unitario é %.2f\n", produtoA, estoqueA, valorA);
+printf("Produto %s tem estoque %u e o valor unitario é %.2f\n", produtoB, estoqueB, valorB);
+
+resultadoA = estoqueA > estoqueminimoA;
+resultadoB = estoqueB > estoqueminimoB;
+
+printf(" O produto %s tem estoque minimo %d\n", produtoA, resultadoA);
+printf(" O produto %s tem estoque minimo %d\n", produtoB, resultadoB);
+
+printf("Valor total de A (R$ %.2f) é maior que o valor total de B(R$ %.2f): %d\n",estoqueA * valorA, estoqueB * valorB,(estoqueA * valorA) > (estoqueB * valorB));
 
 
 
