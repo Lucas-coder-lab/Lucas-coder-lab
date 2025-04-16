34e62cbed8849a3ce763aa491a1098810b5fce43
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-16 22:39:58 +0000

    GerenciadordeEstoque

---
+++ GerenciadordeEstoque.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+ //variaveis para produto, estoque e valores.
+
+char produtoA [30];
+char produtoB [30];
+
+unsigned int estoqueA = 1000;
+unsigned int estoqueB = 2000;
+
+float valorA = 10.50;
+float valorB = 20.40;
+
+unsigned int estoqueMinimoA = 500;
+unsigned int estoqueMinimoA = 2500;
+
+double valorTotalA;
+double valorTotalB;
+
+
+printf("Produto %s tem estoque %u e o valor unitario é %f\n", produtoA, estoqueA, valorA);
+printf("Produto %s tem estoque %u e o valor unitario é %f\n", produtoB, estoqueB, valorB);
+
+
+
+
+return 0;
+}
