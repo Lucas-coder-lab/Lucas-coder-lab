a2eb133db8450ebe30def5bd8b633922063b74e2
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-04-17 00:24:44 +0000

    decisao

---
+++ EstruturadeDecisao.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+
+int a = 5,b = 2;
+int condicao;
+
+condicao = a > b;
+
+printf("O valor logico da condicao %d\n", condicao);
+
+if(a > b)
+{
+  printf(" A e maior que B: %d\n", condicao);
+
+}
+else
+{
+    printf("B e maior que A: %d\n", condicao);
+}
+
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+}
