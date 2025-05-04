73bbdd7d135659852271c91422c66ad05b504e24
Lucas-coder-lab <lucas-henrique_06@outlook.com>
2025-05-04 00:19:02 +0000

    menu

--- EstruturaAninhada.c
+++ EstruturaAninhada.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int entrada(){
+
+
+char nome;
+int CPF;
+int datanascimento;
+float planomed;
+
+    printf("Digite seu nome: \n");
+    scanf("%s\n", nome);
+    printf("Digite seu CPF: \n");
+    scanf("%d\n", &CPF);
+    printf("Digite a data de nascimento \n");
+    scanf("%d\n", &datanascimento);
+    printf("Sera pelo plano de saude? digite apenas sim ou não \n");
+    scanf("%f\n", &planomed);
+
+ return 0;
+}
+
+ 
